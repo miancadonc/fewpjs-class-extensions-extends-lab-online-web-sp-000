a6a967b7e1a97dc0dfe5bac73cc361b4438641a5
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-31 04:14:24 -0500

    commit tree e04d7cfb19b1875ab943fedbc2d14c345dc6a879

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+    constructor(sidesArray){
+        this.sidesArray = sidesArray
+    }
+
+    get countSides(){
+        return this.sidesArray.length
+    }
+
+    get perimeter(){
+        return this.sidesArray.reduce(((side, total)=> side + total),0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if(this.sidesArray.find(e => e > (this.perimeter / 2))){
+            return false
+        }
+        return true
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        if(this.sidesArray.find(e => e !== (this.perimeter / 4))){
+            return false
+        }
+        return true
+    }
+
+    get area(){
+        return (this.perimeter / 4)**2
+    }
+}
